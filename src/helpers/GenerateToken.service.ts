d3511537dc92812bbdfbcc7eaf681f00ccf9df1a
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-01-12 13:12:17 -0300

    feat(token): generate token class

---
+++ src/helpers/GenerateToken.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import jwt from 'jsonwebtoken';
+import { promisify } from 'node:util';
+
+@Injectable()
+export class GenerateToken {
+  secret = 'any_secret';
+  async generate(userId: string) {
+    const token = await promisify(jwt.sign)(userId, this.secret);
+    return token;
+  }
+}
